7ce8cce78f8a985b87fdeb57add1a56f2e441c2e
uhndraeee <162891098+Drei8@users.noreply.github.com>
2026-09-16 10:02:28 +0800

    chore: point the add-on homepage to roscommon.systems

--- buildVars.py
+++ buildVars.py
@@ -34,7 +34,7 @@ addon_info = AddonInfo(
 	# Author(s)
 	addon_author="Roscommon Systems",
 	# URL for the add-on documentation support
-	addon_url="https://github.com/roscommonsystems/lima-ai-nvda-addon",
+	addon_url="https://www.roscommon.systems/",
 	# URL for the add-on repository where the source code can be found
 	addon_sourceURL="https://github.com/roscommonsystems/lima-ai-nvda-addon",
 	# Documentation file name
